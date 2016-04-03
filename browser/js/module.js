b8bee6efd279d83f1f9ef3080746b8e58c0b20ec
John Nieves <karn09@gmail.com>
2016-04-02 20:02:45 -0400

    draggable lists and typeahead

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var juke = angular.module('juke', ['ui.router', 'ui.bootstrap']);
+var juke = angular.module('juke', ['ui.router', 'ui.bootstrap', 'ui.sortable']);
 
 juke.run(function($rootScope) {
   $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
